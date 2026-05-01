a980c7d53326488e42348f0c147e014179505a4c
Leandro Lopes <92340963+leandrolopesr@users.noreply.github.com>
2026-05-01 16:27:53 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -80,9 +80,3 @@ window.addEventListener('scroll', () => {
     });
 });
 
-
-// Apenas a lógica, SEM as tags <script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-3ZCJB18P10');
